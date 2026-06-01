09b0d5d7fc3872b7ab99f2fc33679d24fd26fd66
Jess Friedersdorff <30495397+JessF298@users.noreply.github.com>
2026-06-01 17:02:06 +0100

    feat: add enhaced should_run logic for orange box - in testing.

--- src/cherami/pipelines/orange_box.py
+++ src/cherami/pipelines/orange_box.py
@@ -2,7 +2,9 @@ import csv
 import logging
 from pathlib import Path
 
-from cherami.config import PipelineConfig, WorkerConfig
+from onyx_analysis_helper import onyx_analysis_helper_functions as oa
+
+from cherami.config import GlobalConfig, PipelineConfig, WorkerConfig
 from cherami.pipelines.pipeline import Pipeline
 from cherami.pipelines.worker import Worker
 
@@ -32,6 +34,76 @@ class OrangeBoxPipeline(Pipeline):
             "Generated orange_box samplesheet at %s",
             output_filepath,
         )
+
+    def should_run(self, sample_id: str) -> bool:
+        """
+        Determines whether orangebox should run.
+        Orange Box should run for a sample when either:
+        - no orangebox analysis tables exist for the current configured
+        orangebox version.
+        - the current Onyx upstream context differs from the upstream context
+        stored on orangebox analysis tables for the current configured
+        orangebox version.
+        Arguments:
+            sample_id: str, sample id.
+            server: str, must be valid onyx server.
+        Returns:
+            should_run_decision: should run? - bool True for yes False for no.
+        """
+        analysis_tables, exitcode = oa.get_analysis_records(
+            sample_id=sample_id,
+            server=GlobalConfig.server,
+            fields=["methods"],  # Need to set server
+        )
+
+        if exitcode != 0:
+            logger.error(
+                "Cannot query Onyx for analyses for sample %s." % (sample_id)  # noqa: G002, UP031
+            )
+            return False
+
+        # First get a set of the orange_box_versions and the onyx_version_hashes
+        orange_box_versions = set()
+        onyx_versions_hashes = set()
+
+        for table in analysis_tables.values():
+            # add onyx versions hashes from analysis tables:
+            onyx_versions_hashes.add(table["methods"]["onyx_versions_hash"])
+
+            # Get the orange box version from the analysis tables
+            versions = table["methods"]["versions"]
+            versions_dict = {ver["name"]: ver["version"] for ver in versions}
+            orange_box_versions.add(versions_dict.get("orange_box_version"))
+
+        # Then get the current upstream context:
+        _, current_onyx_versions, exitcode = (
+            oa.get_data_and_versions_from_onyx(
+                sample_id=sample_id,
+                server=GlobalConfig.server,
+                fields=["climb_id"],
+            )
+        )
+        if exitcode != 0:
+            logger.error(
+                "Cannot query Onyx for upstream context, uncertain whether to run, exiting."
+            )
+            return False
+
+        current_onyx_hash = oa._calculate_versions_hash(current_onyx_versions)
+
+        # Get the orange box version:
+        orange_box_version = PipelineConfig.version
+
+        # Then check if all of the current onyx
+
+        if (  # noqa: SIM103
+            orange_box_version in orange_box_versions
+            and current_onyx_hash in onyx_versions_hashes
+        ):
+            # do not rerun if orange box version matches and upstream context matches
+            return False
+        else:
+            return True
 
 
 def build_worker(
