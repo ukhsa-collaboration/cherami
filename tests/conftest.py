90051b0450f5dcc33aeb594f77a11be57e6e2655
Jess Friedersdorff <30495397+JessF298@users.noreply.github.com>
2026-07-08 15:05:40 +0100

    tests: added info to conftest fixtures and unittests for samplesheet for strep pneumo.

--- tests/conftest.py
+++ tests/conftest.py
@@ -65,6 +65,9 @@ ONYX_RECORD = {
     "scylla_version": "1.0.0",
     "sylph_db_version": "1.0.0",
     "alignment_db_version": "1.0.0",
+    "human_filtered_reads_1": "/path/to/forward/fastq",
+    "human_filtered_reads_2": "",
+    "taxon_reports": "path/to/taxon_reports",
 }
 """Mocked onyx client.get response using old onyx format."""
 
